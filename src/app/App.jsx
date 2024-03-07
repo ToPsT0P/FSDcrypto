1e7f37f91c88815aac1f68126e5b6c9976f4be81
ToPsT0P <pustovalov.al.an@gmail.com>
2024-03-07 08:09:14 +0300

    Создание и инициализация проекта

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import MainPage from '../pages/MainPage/MainPage'
+function App() {
+
+  return (
+    <>
+      <MainPage/>
+    </>
+  )
+}
+
+export default App
