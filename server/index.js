f8e4c5c9d8c4e04f145704c1b514237799b19ce8
Alexander Pustovalov <Pustovalov.al.an@gmail.com>
2024-03-21 15:36:36 +0300

    стартовая версия backend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("as")
+require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        app.listen(PORT, () => console.log(`Порт на котором ебашит = ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
